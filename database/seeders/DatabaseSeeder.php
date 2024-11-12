f589a2442b1e4417cea3f9b8f0fa0449467808ae
Aditya <4adi26@gmail.com>
2024-11-12 20:25:17 +0530

    "Updated DatabaseSeeder to create job types and categories"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Category;
+use App\Models\JobType;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,20 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        collect(['Full Time', 'Part Time', 'Internship', 'Freelance', 'Remote'])->each(function ($type) {
+            JobType::factory()->create(['name' => $type]);
+        });
+
+
+        collect(['Accountant', 'Human Resources', 'IT', 'Marketing', 'Sales'])->each(function ($category) {
+            Category::factory()->create(['name' => $category]);
+        });
+
+
     }
 }
